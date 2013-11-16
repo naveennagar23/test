7497c4b645b86b55637956113e7cd8d4f35583a9
naveen <nagar.naveen23@gmail.com>
2013-11-16 00:12:27 -0800

    test1

---
+++ contacts.php
@@ -0,0 +1,49 @@
+<?php include('head.php');?>
+<body>
+<div id="container">
+  <?php include('slideshow.php');?> 
+<?php include('menubar.php');?>  
+
+  <div id="content"><br/><br/>
+    <div id="maincontent"><p >
+    <h5>Indian Progressive Society(IPS)</h5>
+     <p ><table width="600" height="300
+      " border="0" cellpadding="0" cellspacing="0">
+    <tr>
+    <td>&nbsp;<b>Contact Person</b></td>
+    <td>&nbsp;Neeraj Malik</td>
+  </tr>
+  <tr>
+    <td><b> &nbsp;Address
+    
+   </b> </td>
+    <td>&nbsp;Ramnagar, Roorkee -247667
+                     Uttarakhand</td>
+  </tr>
+  <tr>
+    <td>&nbsp;<b>Contact Number</b></td>
+    <td>&nbsp;09690386700</td>
+  </tr>
+  
+  <tr>
+    <td><b>&nbsp;Email</b></td>
+    <td>&nbsp;ipsworld@yahoo.com <br/>&nbsp; indianprogressivesociety@gmail.com</td>
+  </tr>
+</table>
+
+  
+
+    </p> 
+
+    </div>
+
+      <?php include('sidebar.php');?>
+  
+  </div>
+
+    <?php include('footer.php');?>
+</div>
+   
+
+</body>
+</html>
